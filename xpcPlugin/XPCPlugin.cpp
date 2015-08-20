e932179e20dd7c5f27b306534955d55b3fa77096
Jason Watkins <jason-watkins@outlook.com>
2015-08-20 08:12:20 -0700

    Updated plugin version number and new Windows binaries

--- xpcPlugin/XPCPlugin.cpp
+++ xpcPlugin/XPCPlugin.cpp
@@ -91,7 +91,7 @@ static float XPCFlightLoopCallback(float inElapsedSinceLastCall, float inElapsed
 
 PLUGIN_API int XPluginStart(char* outName, char* outSig, char* outDesc)
 {
-	strcpy(outName, "X-Plane Connect [Version 1.1.1]");
+	strcpy(outName, "X-Plane Connect [Version 1.2.0]");
 	strcpy(outSig, "NASA.XPlaneConnect");
 	strcpy(outDesc, "X Plane Communications Toolbox\nCopyright (c) 2013-2015 United States Government as represented by the Administrator of the National Aeronautics and Space Administration. All Rights Reserved.");
 
