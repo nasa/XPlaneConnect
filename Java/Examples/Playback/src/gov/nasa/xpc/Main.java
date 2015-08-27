591e3277d513d86a3bf905e9121288f168310aed
Jason Watkins <jason-watkins@outlook.com>
2015-08-27 02:44:37 -0700

    Fixed input parsing in Java playback example.

--- Java/Examples/Playback/src/gov/nasa/xpc/Main.java
+++ Java/Examples/Playback/src/gov/nasa/xpc/Main.java
@@ -100,7 +100,7 @@ public class Main
                 for (int i = 0; i < count; ++i)
                 {
                     float[] posi = xpc.getPOSI(0);
-                    writer.write(String.format("%1$f , %2$f , %3$f , %4$f , %5$f , %6$f , %7$f\n",
+                    writer.write(String.format("%1$f, %2$f, %3$f, %4$f, %5$f, %6$f, %7$f\n",
                             posi[0], posi[1], posi[2], posi[3], posi[4], posi[5], posi[6]));
                     try
                     {
@@ -119,6 +119,7 @@ public class Main
     {
         try(Scanner reader = new Scanner(new FileReader(path)))
         {
+            reader.useDelimiter("[\\s\\r\\n,]+");
             System.out.println("Starting playback...");
             try (XPlaneConnect xpc = new XPlaneConnect())
             {
@@ -127,8 +128,8 @@ public class Main
                     float[] posi = new float[7];
                     for (int i = 0; i < 7; ++i)
                     {
-                        posi[i] = reader.nextFloat();
-                        reader.next(); // Read commas to skip them
+                        String s = reader.next();
+                        posi[i] = Float.parseFloat(s);
                     }
                     reader.nextLine();
                     xpc.sendPOSI(posi);
