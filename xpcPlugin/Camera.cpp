86b26755fff73ba2c19129a9e8afe49c3c601d6d
Dr Kai Lehmkühler <kai.lehmkuehler@lilium.com>
2019-01-13 13:53:22 +0100

    add files for camera functions

---
+++ xpcPlugin/Camera.cpp
@@ -0,0 +1,8 @@
+//
+//  Camera.cpp
+//  xpcPlugin
+//
+//  Created by Kai Lehmkuehler on 13/01/2019.
+//
+
+#include "Camera.h"
---
+++ xpcPlugin/Camera.h
@@ -0,0 +1,18 @@
+//
+//  Camera.h
+//  xpcPlugin
+//
+//  Created by Kai Lehmkuehler on 13/01/2019.
+//
+
+#ifndef Camera_h
+#define Camera_h
+
+#include <cstdlib>
+
+namespace XPC
+{
+    
+}
+
+#endif /* Camera_h */
