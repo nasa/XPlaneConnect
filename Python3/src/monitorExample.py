e1d4365fa077e89a8f596354cba87b43caf34eb1
Stuart MacIntosh <stuart@macintosh.nz>
2019-12-12 14:32:25 +1300

    Python3 demos using Python2 syntax were run through 2to3, fixes #191

--- Python3/src/monitorExample.py
+++ Python3/src/monitorExample.py
@@ -8,8 +8,8 @@ def monitor():
             posi = client.getPOSI();
             ctrl = client.getCTRL();
 
-            print "Loc: (%4f, %4f, %4f) Aileron:%2f Elevator:%2f Rudder:%2f\n"\
-               % (posi[0], posi[1], posi[2], ctrl[1], ctrl[0], ctrl[2])
+            print("Loc: (%4f, %4f, %4f) Aileron:%2f Elevator:%2f Rudder:%2f\n"\
+               % (posi[0], posi[1], posi[2], ctrl[1], ctrl[0], ctrl[2]))
 
 
 if __name__ == "__main__":
