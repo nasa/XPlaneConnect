6a483db8cc4a94415a84e43d1eb1a78756e7f265
Norman Princen <63923352+NPrincen@users.noreply.github.com>
2020-04-19 10:46:59 -0700

    Python3 demos using Python2 syntax were run through 2to3, fixes nasa#191

--- Python3/src/basicExample.py
+++ Python3/src/basicExample.py
@@ -2,8 +2,8 @@ from time import sleep
 import xpc
 
 def ex():
-    print "X-Plane Connect example script"
-    print "Setting up simulation"
+    print("X-Plane Connect example script")
+    print("Setting up simulation")
     with xpc.XPlaneConnect() as client:
         # Verify connection
         try:
@@ -11,24 +11,24 @@ def ex():
             # will be raised.
             client.getDREF("sim/test/test_float")
         except:
-            print "Error establishing connection to X-Plane."
-            print "Exiting..."
+            print("Error establishing connection to X-Plane.")
+            print("Exiting...")
             return
 
         # Set position of the player aircraft
-        print "Setting position"
+        print("Setting position")
         #       Lat     Lon         Alt   Pitch Roll Yaw Gear
         posi = [37.524, -122.06899, 2500, 0,    0,   0,  1]
         client.sendPOSI(posi)
         
         # Set position of a non-player aircraft
-        print "Setting NPC position"
+        print("Setting NPC position")
         #       Lat       Lon         Alt   Pitch Roll Yaw Gear
         posi = [37.52465, -122.06899, 2500, 0,    20,   0,  1]
         client.sendPOSI(posi, 1)
 
         # Set angle of attack, velocity, and orientation using the DATA command
-        print "Setting orientation"
+        print("Setting orientation")
         data = [\
             [18,   0, -998,   0, -998, -998, -998, -998, -998],\
             [ 3, 130,  130, 130,  130, -998, -998, -998, -998],\
@@ -37,21 +37,21 @@ def ex():
         client.sendDATA(data)
 
         # Set control surfaces and throttle of the player aircraft using sendCTRL
-        print "Setting controls"
+        print("Setting controls")
         ctrl = [0.0, 0.0, 0.0, 0.8]
         client.sendCTRL(ctrl)
 
         # Pause the sim
-        print "Pausing"
+        print("Pausing")
         client.pauseSim(True)
         sleep(2)
 
         # Toggle pause state to resume
-        print "Resuming"
+        print("Resuming")
         client.pauseSim(False)
 
         # Stow landing gear using a dataref
-        print "Stowing gear"
+        print("Stowing gear")
         gear_dref = "sim/cockpit/switches/gear_handle_status"
         client.sendDREF(gear_dref, 0)
 
@@ -61,12 +61,12 @@ def ex():
         # Make sure gear was stowed successfully
         gear_status = client.getDREF(gear_dref)
         if gear_status[0] == 0:
-            print "Gear stowed"
+            print("Gear stowed")
         else:
-            print "Error stowing gear"
+            print("Error stowing gear")
 
-        print "End of Python client example"
-        raw_input("Press any key to exit...")
+        print("End of Python client example")
+        input("Press any key to exit...")
 
 if __name__ == "__main__":
     ex()
