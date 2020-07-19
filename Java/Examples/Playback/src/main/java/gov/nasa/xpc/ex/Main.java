5a981441708104044d9b618be00f7bd4e00033bb
Mike Frizzell <>
2020-07-19 15:08:29 -0500

    re-org and get compiling

--- Java/Examples/Playback/src/main/java/gov/nasa/xpc/ex/Main.java
+++ Java/Examples/Playback/src/main/java/gov/nasa/xpc/ex/Main.java
@@ -1,6 +1,8 @@
-package gov.nasa.xpc;
+package gov.nasa.xpc.ex;
 
 import com.sun.javafx.scene.EnteredExitedHandler;
+
+import gov.nasa.xpc.XPlaneConnect;
 
 import java.io.*;
 import java.util.Scanner;
@@ -99,7 +101,7 @@ public class Main
             {
                 for (int i = 0; i < count; ++i)
                 {
-                    float[] posi = xpc.getPOSI(0); // FIXME: change this to 64-bit double
+                    double[] posi = xpc.getPOSI(0); // FIXME: change this to 64-bit double
                     writer.write(String.format("%1$f, %2$f, %3$f, %4$f, %5$f, %6$f, %7$f\n",
                             posi[0], posi[1], posi[2], posi[3], posi[4], posi[5], posi[6]));
                     try
