8ad328c26f4ae63cbf966b5aaca3f83a7251dc0d
Werner Duvaud <40442230+werner-duvaud@users.noreply.github.com>
2021-04-30 01:49:17 +0200

    Add setup.py and move file

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='xplaneconnect',
+    version='0.1',
+    packages=setuptools.find_packages("xplaneconnect"),
+    author='',
+    author_email='',
+    url='',
+    python_requires='>=3.6',
+)
