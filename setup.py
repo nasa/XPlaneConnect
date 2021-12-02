791f225bd25ef2431febe608d436237980e88076
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-02 20:38:34 +0100

    Unifying quotation marks and removing comment

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     package_dir={"": "Python3/src"},
     description="XPlaneConnect (XPC) facilitates communication to and from the XPCPlugin. Note: To use the XPC package, you must first install the XPCPlugin. See https://github.com/nasa/XPlaneConnect/wiki/Getting-Started for instructions on installing the XPCPlugin",
     url="https://github.com/nasa/XPlaneConnect/",
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/nasa/xplaneconnect/issues',
-        'Organization': 'https://www.nasa.gov/content/diagnostics-prognostics',
-        'Source': 'https://github.com/nasa/xplaneconnect',
-        'Documentation': 'https://github.com/nasa/XPlaneConnect/wiki'
+    project_urls={
+        "Bug Reports": "https://github.com/nasa/xplaneconnect/issues",
+        "Organization": "https://www.nasa.gov/content/diagnostics-prognostics",
+        "Source": "https://github.com/nasa/xplaneconnect",
+        "Documentation": "https://github.com/nasa/XPlaneConnect/wiki"
     }
 )
