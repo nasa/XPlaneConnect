6acb765bd01a35b24578eb7cc619d1ec6d951cce
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-12-02 20:32:05 +0100

    Apply suggestions from code review
    Co-authored-by: Christopher Teubert <christopher.a.teubert@nasa.gov>

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ setup(
     python_requires=">=3",
     packages=find_packages("Python3/src"),
     package_dir={"": "Python3/src"},
-    description="XPlaneConnect (XPC) facilitates communication to and from the XPCPlugin.",
+    description="XPlaneConnect (XPC) facilitates communication to and from the XPCPlugin. Note: To use the XPC package, you must first install the XPCPlugin. See https://github.com/nasa/XPlaneConnect/wiki/Getting-Started for instructions on installing the XPCPlugin",
     url="https://github.com/nasa/XPlaneConnect/",
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/nasa/xplaneconnect/issues',
+        'Organization': 'https://www.nasa.gov/content/diagnostics-prognostics',
+        'Source': 'https://github.com/nasa/xplaneconnect',
+        'Documentation': 'https://github.com/nasa/XPlaneConnect/wiki'
+    }
 )
