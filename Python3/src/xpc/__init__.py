c83b968e3f77871e4a8c8ab419d9b44841326db4
Jaaaky <43599380+Jaaaky@users.noreply.github.com>
2022-01-24 21:47:32 +0300

    Python3: Import sendCOMM to python3

--- Python3/src/xpc/__init__.py
+++ Python3/src/xpc/__init__.py
@@ -425,6 +425,27 @@ class XPlaneConnect(object):
             buffer = struct.pack(("<4sxBB" + str(len(points)) + "f").encode(), b"WYPT", op, len(points), *points)
         self.sendUDP(buffer)
 
+    #      Send commands
+    def sendCOMM(self, comm):
+        '''Send commands
+
+            Args:
+            command string to execute
+        '''
+        if comm == None:
+            raise ValueError("comm must be non-empty.")
+
+        buffer = struct.pack(b"<4sx", b"COMM")
+        if len(comm) == 0 or len(comm) > 255:
+            raise ValueError("comm must be a non-empty string less than 256 characters.")
+
+        # Pack message
+        fmt = "<B{0:d}s".format(len(comm))
+        buffer += struct.pack(fmt, len(comm), comm.encode())
+
+        # Send
+        self.sendUDP(buffer)
+
 
 class ViewType(object):
     Forwards = 73
